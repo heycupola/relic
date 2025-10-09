85d6e18346f65204a0cf59e4d3671db5f565337a
Can Vardar <ismailcanvardar@gmail.com>
2025-10-09 21:38:51 +0300

    feat: implement tailwind and shadcn

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,9 +4,15 @@
 // import { Authenticated, Unauthenticated, AuthLoading } from "convex/react";
 // import { authClient } from "@/lib/auth";
 
+import { Input } from "@repo/ui/components/input";
+
 
 export default function Home() {
   return (
-    <h1>This is heading.</h1>
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <Input type="email" placeholder="Email" />
+      <h1>This is heading.</h1>
+      <p className="font-black text-blue-600">hellot</p>
+    </div>
   );
 }
