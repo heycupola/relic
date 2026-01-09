b22b95417ffc239b7e517b70fbd84aebd8c1ed84
Can Vardar <ismailcanvardar@gmail.com>
2026-01-09 13:26:47 +0300

    chore(backend): update access restricted email template

--- packages/backend/convex/lib/emails/access_restricted.tsx
+++ packages/backend/convex/lib/emails/access_restricted.tsx
@@ -1,4 +1,5 @@
 import { Body, Button, Container, Head, Hr, Html, Section, Text } from "@react-email/components";
+import * as React from "react";
 
 interface AccessRestrictedEmailProps {
   userName?: string;
