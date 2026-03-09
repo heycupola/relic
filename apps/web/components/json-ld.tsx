57b69b870a5b51d4a1fdefbc9eee8c6f85c0d567
Can Vardar <ismailcanvardar@gmail.com>
2026-03-09 12:05:02 +0300

    fix(web): escape angle brackets in JsonLd to prevent XSS

--- apps/web/components/json-ld.tsx
+++ apps/web/components/json-ld.tsx
@@ -4,6 +4,9 @@ interface JsonLdProps {
 
 export function JsonLd({ data }: JsonLdProps) {
   return (
-    <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }} />
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(data).replace(/</g, "\\u003c") }}
+    />
   );
 }
