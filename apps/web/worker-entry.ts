100ab8ffcd49b63fb0bfdf6970d819410c54395a
Can Vardar <ismailcanvardar@gmail.com>
2026-03-17 20:47:35 +0300

    fix(web): allow PostHog assets domain in CSP script-src

--- apps/web/worker-entry.ts
+++ apps/web/worker-entry.ts
@@ -7,7 +7,7 @@ const SECURITY_HEADERS = {
   "Referrer-Policy": "strict-origin-when-cross-origin",
   "Content-Security-Policy": [
     "default-src 'self'",
-    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://us.i.posthog.com",
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://us.i.posthog.com https://us-assets.i.posthog.com",
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
     "img-src 'self' data: https:",
     "font-src 'self' data: https://fonts.gstatic.com",
